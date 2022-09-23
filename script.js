e836b81f45179e839442e737f632c8430a7ac25a
Andrea <andr45f9@stud.kea.dk>
2022-09-23 13:57:43 +0200

    expell student is working properly!

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const url = "https://petlatkea.dk/2021/hogwarts/students.json";
 //array where the new object should get stored in
 const allStudents = [];
 
+//array where we can add the students who gets expelled
+const expelledStudents = [];
+
 //This variable will later on hold the json files.
 let list;
 
@@ -23,7 +26,7 @@ const Student = {
   blood: "",
   star: false,
   trophy: false,
-  expelled: false,
+  isExpelled: false,
   crest: "",
 };
 
@@ -44,6 +47,8 @@ function addingEventListeners() {
   document.querySelectorAll("[data-action='filter']").forEach((option) => option.addEventListener("click", selectFilter));
 
   document.querySelectorAll("[data-action='sort']").forEach((option) => option.addEventListener("click", selectSort));
+
+  document.querySelectorAll("[data-action='search']").forEach((option) => option.addEventListener("input", selectSearch));
 }
 
 async function loadJSON() {
@@ -86,6 +91,7 @@ function prepareStudents(list) {
     //Getting the LAST NAME and making it upper case.
     student.lastName = fullName.substring(fullName.lastIndexOf(" ") + 1, fullName.lastIndexOf(" ") + 2).toUpperCase() + fullName.substring(fullName.lastIndexOf(" ") + 2).toLowerCase();
     //last name with a hyphen included --- have to do ---
+    //no last name  --- have to do ---
 
     //Getting the GENDER and making it upper case.
     student.gender = gender.substring(0, 1).toUpperCase() + gender.substring(1).toLowerCase();
@@ -95,6 +101,7 @@ function prepareStudents(list) {
 
     //Getting the IMAGE URL by the lastname in lowercase + _ + the first letter of the first name and the .png.
     student.image = fullName.substring(fullName.lastIndexOf(" ")).trim().toLowerCase() + "_" + fullName.substring(0, 1).toLowerCase() + ".png";
+    //getting img url with full first name  --- have to do ---
 
     //Getting the HOUSE CREST IMAGE URL
     student.crest = house.substring(0, 1).toUpperCase() + house.substring(1).toLowerCase() + ".png";
@@ -104,9 +111,15 @@ function prepareStudents(list) {
   });
 
   //Displaying the students array as a table in the console.
-  console.table(allStudents);
+  //console.table(allStudents);
 
-  displayStudent(allStudents);
+  showInfo();
+  buildList();
+}
+
+//------ Practical infor displayed -----------//
+function showInfo() {
+  //console.log("yaaaas");
 }
 
 //------ALL filtering----------- Filtering by house or expelled students/non-expelled and all students.
@@ -121,6 +134,7 @@ function selectFilter(event) {
 
 function setFilter(filter) {
   settings.filterBy = filter;
+
   buildList();
 }
 
@@ -138,7 +152,7 @@ function filterList(filteredList) {
   } else if (settings.filterBy === "Prefect") {
     filteredList = allStudents.filter(isPrefect);
   } else if (settings.filterBy === "Expelled") {
-    filteredList = allStudents.filter(isExpelled);
+    filteredList = expelledStudents.filter(isExpelled);
   } else if (settings.filterBy === "NonExpelled") {
     filteredList = allStudents.filter(isNonExpelled);
   }
@@ -163,15 +177,19 @@ function isSlytherin(student) {
 }
 
 function isPrefect(student) {
-  return student.trophy === "Prefect";
+  if (student.trophy === true) {
+    student.trophy === "Prefect";
+  }
+
+  return student.trophy;
 }
 
 function isExpelled(student) {
-  return student.expelled === "Expelled";
+  return student.isExpelled;
 }
 
 function isNonExpelled(student) {
-  return student.expelled === "NonExpelled";
+  return !student.isExpelled;
 }
 
 //------ALL sorting----------- Sort by firt and last name from a-z and z-a and student in same house.
@@ -211,10 +229,45 @@ function sortList(sortedList) {
   return sortedList;
 }
 
+//------ALL search bar----------- Search on firstname and lastname.
+
+function selectSearch(event) {
+  let search = event.target.value;
+
+  setSearch(search);
+}
+
+function setSearch(search) {
+  search = search.toLowerCase();
+
+  console.log(search);
+  buildList();
+}
+
+/* function searchList(searchedList) {
+  if (search === "Firstname") {
+    searchedList = allStudents.filter(isFirstname);
+  } else if (search === "Lastname") {
+    searchedList = allStudents.filter(isLastname);
+  }
+  return searchedList;
+}
+
+function isFirstname(student) {
+  return student.firstName === "Firstname";
+}
+
+function isLastname(student) {
+  return student.lastName === "Lastname";
+} */
+
 //------Displaying all the students in list view and popup-----------//
 function buildList() {
   let currentList = filterList(allStudents);
   currentList = sortList(currentList);
+
+  //Amount of students currently displayed (number cahnges everytime the list gets changed)
+  document.querySelector(".amount_displayed").textContent = currentList.length;
 
   displayStudent(currentList);
 }
@@ -240,13 +293,13 @@ function displayStudent(allStudents) {
     clone.querySelector("[data-field=star]").addEventListener("click", clickStar);
 
     function clickStar() {
-      if (animal.star === true) {
-        animal.star = false;
+      if (student.star === true) {
+        student.star = false;
       } else {
-        animal.star = true;
+        student.star = true;
       }
 
-      //buildList();
+      buildList();
     }
 
     // set clone data for TROPHY
@@ -257,12 +310,47 @@ function displayStudent(allStudents) {
       if (student.trophy === true) {
         student.trophy = false;
       } else {
-        //tryToMakeATrophyr(student);
+        tryToMakeAWinner(student);
       }
 
-      //buildList();
+      buildList();
     }
-    clone.querySelector("tr").addEventListener("click", () => showPopUp(student));
+
+    // set clone data for EXPELL button
+    if (student.isExpelled) {
+      clone.querySelector(".expel_button").textContent = "Expelled";
+    } else {
+      clone.querySelector(".expel_button").textContent = "Expel";
+    }
+
+    clone.querySelector(".expel_button").addEventListener("click", clickExpelBtn);
+
+    function clickExpelBtn() {
+      student.isExpelled = !student.isExpelled;
+
+      //find student's position in allStudents
+      const index = allStudents.findIndex((element) => {
+        if (element === student) {
+          return true;
+        }
+        return false;
+      });
+
+      //use student's position to splice student fra allStudents
+      if (index !== -1) {
+        const splicedArray = allStudents.splice(index, 1);
+        const foundElement = splicedArray[0];
+
+        //add student to expelledStudents
+        expelledStudents.push(foundElement);
+      }
+      console.log("array1", allStudents);
+      console.log("array2", expelledStudents);
+
+      buildList();
+    }
+
+    clone.querySelector("[data-field=fullname]").addEventListener("click", () => showPopUp(student));
     container.appendChild(clone);
   });
 }
@@ -285,5 +373,119 @@ function showPopUp(student) {
   popup.querySelector(".inquisitorial").textContent = `Inquisitorial Squad: ${student.star}`;
   popup.querySelector(".crest_pic").src = `crest_img/${student.crest}`;
 
+  //Making sure that each student gets a popup window with the right colors of their crest/house.
+  if (student.house === "Gryffindor") {
+    document.querySelector("#popUp article").style.backgroundColor = "#6C1A32";
+    document.querySelector(".popup_info_1").style.backgroundColor = "#D77E02";
+    document.querySelector(".popup_info_2").style.backgroundColor = "#D77E02";
+    document.querySelector("#close_button").style.backgroundColor = "#D77E02";
+  } else if (student.house === "Hufflepuff") {
+    document.querySelector("#popUp article").style.backgroundColor = "#BB7624";
+    document.querySelector(".popup_info_1").style.backgroundColor = "#EDAB54";
+    document.querySelector(".popup_info_2").style.backgroundColor = "#EDAB54";
+    document.querySelector("#close_button").style.backgroundColor = "#EDAB54";
+  } else if (student.house === "Ravenclaw") {
+    document.querySelector("#popUp article").style.backgroundColor = "#063F53";
+    document.querySelector(".popup_info_1").style.backgroundColor = "#7C9ABF";
+    document.querySelector(".popup_info_2").style.backgroundColor = "#7C9ABF";
+    document.querySelector("#close_button").style.backgroundColor = "#7C9ABF";
+  } else if (student.house === "Slytherin") {
+    document.querySelector("#popUp article").style.backgroundColor = "#164731";
+    document.querySelector(".popup_info_1").style.backgroundColor = "#838E86";
+    document.querySelector(".popup_info_2").style.backgroundColor = "#838E86";
+    document.querySelector("#close_button").style.backgroundColor = "#838E86";
+  }
+
   document.querySelector("#close_button").addEventListener("click", () => (popUp.style.display = "none"));
+}
+
+function tryToMakeAWinner(selectedStudent) {
+  const trophies = allStudents.filter((student) => student.trophy === true);
+  const numberOfTrophies = trophies.length;
+  const other = trophies.filter((student) => student.gender === selectedStudent.gender).shift();
+
+  //check if there is another of the same type selected.
+  if (other !== undefined) {
+    removeOther(other);
+  } else if (numberOfTrophies >= 2) {
+    removeAorB(trophies[0], trophies[1]);
+  } else {
+    makeWinner(selectedStudent);
+  }
+
+  function removeOther(other) {
+    //ask the user to ignore or remove the "other".
+    document.querySelector("#remove_other").classList.remove("hide");
+    document.querySelector("#remove_other .close_button").addEventListener("click", closeDialog);
+    document.querySelector("#remove_other #removeother_button").addEventListener("click", clickRemoveOther);
+
+    //Show the selected animal name on the button:
+    document.querySelector("#remove_other [data-field=otherwinner]").textContent = other.name;
+
+    //if user ignores then do nothing..
+    function closeDialog() {
+      //adding hide so the dialog box will be hidden when the user close the dialog box:
+      document.querySelector("#remove_other").classList.add("hide");
+      //remembering to remove eventListeners again:
+      document.querySelector("#remove_other .close_button").removeEventListener("click", closeDialog);
+      document.querySelector("#remove_other #removeother_button").removeEventListener("click", clickRemoveOther);
+    }
+
+    //if remove the "other" do this:
+    function clickRemoveOther() {
+      removeWinner(other);
+      makeWinner(selectedStudent);
+
+      buildList();
+      closeDialog();
+    }
+  }
+
+  function removeAorB(winnerA, winnerB) {
+    //ask the user to ignore or remove A or B.
+    document.querySelector("#remove_aorb").classList.remove("hide");
+    document.querySelector("#remove_aorb .close_button").addEventListener("click", closeDialog);
+    document.querySelector("#remove_aorb #remove_a").addEventListener("click", clickRemoveA);
+    document.querySelector("#remove_aorb #remove_b").addEventListener("click", clickRemoveB);
+
+    //Show the selected animals names on the buttons:
+    document.querySelector("#remove_aorb [data-field=winnerA]").textContent = winnerA.firstName;
+    document.querySelector("#remove_aorb [data-field=winnerB]").textContent = winnerB.firstName;
+
+    //if user ignores then do nothing..
+    function closeDialog() {
+      //adding hide so the dialog box will be hidden when the user close the dialog box:
+      document.querySelector("#remove_aorb").classList.add("hide");
+      //remembering to remove eventListeners again:
+      document.querySelector("#remove_aorb .close_button").removeEventListener("click", closeDialog);
+      document.querySelector("#remove_aorb #remove_a").removeEventListener("click", clickRemoveA);
+      document.querySelector("#remove_aorb #remove_b").removeEventListener("click", clickRemoveB);
+    }
+
+    //if removeA do this:
+    function clickRemoveA() {
+      removeWinner(winnerA);
+      makeWinner(selectedStudent);
+
+      buildList();
+      closeDialog();
+    }
+
+    //else if removeB then:
+    function clickRemoveB() {
+      removeWinner(winnerB);
+      makeWinner(selectedStudent);
+
+      buildList();
+      closeDialog();
+    }
+  }
+
+  function removeWinner(winnerStudent) {
+    winnerStudent.trophy = false;
+  }
+
+  function makeWinner(student) {
+    student.trophy = true;
+  }
 }
